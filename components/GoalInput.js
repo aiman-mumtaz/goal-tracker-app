b8bd9fae5ba7f44165054efa6b3405b883f5eeb0
Aiman <apple@192.168.1.9>
2025-12-12 21:16:23 +0530

    Revert Backend Code

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,33 +1,44 @@
-import { useState } from "react";
-import { View, TextInput, Button, StyleSheet, Modal, Image } from "react-native";
+import { useState } from 'react';
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  Modal,
+  Image,
+} from 'react-native';
 
 function GoalInput(props) {
-  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [enteredGoalText, setEnteredGoalText] = useState('');
 
   function goalInputHandler(enteredText) {
     setEnteredGoalText(enteredText);
   }
+
   function addGoalHandler() {
     props.onAddGoal(enteredGoalText);
-    setEnteredGoalText("");
+    setEnteredGoalText('');
   }
 
   return (
     <Modal visible={props.visible} animationType="slide">
       <View style={styles.inputContainer}>
-        <Image source={require('../assets/images/goal.png')} style={styles.image} />
+        <Image
+          style={styles.image}
+          source={require('../assets/images/goal.png')}
+        />
         <TextInput
           style={styles.textInput}
-          placeholder="Enter your course goal"
+          placeholder="Your course goal!"
           onChangeText={goalInputHandler}
           value={enteredGoalText}
         />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
-            <Button title="Add Goal" onPress={addGoalHandler} color='#b180f0' />
+            <Button title="Cancel" onPress={props.onCancel} color="#f31282" />
           </View>
           <View style={styles.button}>
-            <Button title="Cancel" onPress={props.onCancel} color="#f31282" />
+            <Button title="Add Goal" onPress={addGoalHandler} color="#b180f0" />
           </View>
         </View>
       </View>
@@ -40,14 +51,10 @@ export default GoalInput;
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     padding: 16,
-    backgroundColor: "#311b6b",
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    marginTop: 16,
+    backgroundColor: '#311b6b',
   },
   image: {
     width: 100,
@@ -56,14 +63,16 @@ const styles = StyleSheet.create({
   },
   textInput: {
     borderWidth: 1,
-    // borderColor: "#e4d0ff",
-    // backgroundColor: "#e4d0ff",
-    borderColor: "#ffffff",
-    backgroundColor: "#ffffff",
-    color: "#120438",
+    borderColor: '#e4d0ff',
+    backgroundColor: '#e4d0ff',
+    color: '#120438',
     borderRadius: 6,
-    width: "100%",
+    width: '100%',
     padding: 16,
+  },
+  buttonContainer: {
+    marginTop: 16,
+    flexDirection: 'row',
   },
   button: {
     width: 100,
--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,41 +1,15 @@
-import { useState } from 'react';
-import { StyleSheet, View, Text, Pressable, Modal, Button } from 'react-native';
-function GoalItem(props) {
+import { StyleSheet, View, Text, Pressable } from 'react-native';
 
-  const [optionModalVisible, setOptionModalVisible] = useState(false);
-  const [isCompleted, setIsCompleted] = useState(false);
-  const openOptions = () => {
-    setOptionModalVisible(true);
-  };
-  const markAsCompleteToggle = () => {
-    setIsCompleted(!isCompleted);
-    setOptionModalVisible(false);
-  }
+function GoalItem(props) {
   return (
-    <View style={[styles.goalItem, isCompleted && styles.completedItem]}>
+    <View style={styles.goalItem}>
       <Pressable
         android_ripple={{ color: '#210644' }}
-        // onPress={props.onDeleteItem.bind(this, props.id)}
-        onLongPress={openOptions}
+        onPress={props.onDeleteItem.bind(this, props.id)}
         style={({ pressed }) => pressed && styles.pressedItem}
       >
         <Text style={styles.goalText}>{props.text}</Text>
       </Pressable>
-      <View>
-          <Modal visible={optionModalVisible} animationType="fade" transparent={true}>
-            <View style={styles.optionsContainer}>
-                <View style={styles.optionItem}>
-                  <Button title="Delete Goal" onPress={props.onDeleteItem.bind(this, props.id)} color='#c11010ff' />
-                </View>
-                <View style={styles.optionItem}>
-                  <Button title={isCompleted ? "Mark In Progress" : "Mark Complete"} onPress={markAsCompleteToggle} color={isCompleted ? '#e47b25ff' : '#228B22'} />
-                </View>
-                <View style={styles.optionItem}>
-                  <Button title="Cancel" onPress={() => setOptionModalVisible(false)} color='#b180f0' />
-                </View>
-            </View>
-          </Modal>
-      </View>
     </View>
   );
 }
@@ -44,12 +18,9 @@ export default GoalItem;
 
 const styles = StyleSheet.create({
   goalItem: {
-    marginVertical: 8,
+    margin: 8,
     borderRadius: 6,
     backgroundColor: '#5e0acc',
-  },
-  completedItem: {
-    backgroundColor: '#228B22',
   },
   pressedItem: {
     opacity: 0.5,
@@ -58,16 +29,4 @@ const styles = StyleSheet.create({
     color: 'white',
     padding: 8,
   },
-  optionsContainer: {
-    flex: 1,
-    backgroundColor: 'transparent',
-    borderRadius: 6,
-    padding: 24,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  optionItem: {
-    width: '80%',
-    backgroundColor: '#fff',
-  }
 });
